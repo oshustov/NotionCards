28e5bb684eb5382f00febffc94c804210dbba997
Oleksandr Shustov <shustov.sanya@gmail.com>
2024-06-14 22:22:53 +0300

    add basic page

---
+++ src/NotionCards/notion-cards.web-app/src/sets/AllSets.js
@@ -0,0 +1,13 @@
+import './AllSets.css'
+import Set from './Set';
+
+function AllSets() {
+  return (
+    <>
+      <Set></Set>
+      <Set></Set>
+    </>
+  );
+}
+
+export default AllSets;
---
+++ src/NotionCards/notion-cards.web-app/src/sets/Set.js
@@ -0,0 +1,13 @@
+import './Set.css'
+
+export default function Set() {
+  return (
+    <div class="set-body" onClick={() => alert('clicked')}>
+      <div>Some name</div>
+      <br></br>
+      <span>
+        454 cards
+      </span>
+    </div>
+  );
+}
